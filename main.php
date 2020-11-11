50718eefa8b3836d32ae5f085cce8aaf76faa001
Cavan Scoffin-Thomas <cavanst@outlook.com>
2020-11-11 16:39:32 +0000

    accept json input and convert to array

--- main.php
+++ main.php
@@ -1,1 +1,10 @@
+#!/usr/bin/php
 <?php
+
+$arr = json_decode($argv[1], true);
+print_r($arr);
+
+die();
+
+// example input
+// "[{\"from\": \"Adolfo Suárez Madrid–Barajas Airport, Spain\",\"to\": \"London Heathrow, UK\"},{\"from\": \"Fazenda São Francisco Citros, Brazil\",\"to\": \"São Paulo–Guarulhos International Airport, Brazil\"},{\"from\": \"London Heathrow, UK\",\"to\": \"Loft Digital, London, UK\"},{\"from\": \"Porto International Airport, Portugal\",\"to\": \"Adolfo Suárez Madrid–Barajas Airport, Spain\"},{\"from\": \"São Paulo–Guarulhos International Airport, Brazil\",\"to\": \"Porto International Airport, Portugal\"}]"
