643218f75a29694cb150670f3e5e83c500e3949a
Cavan Scoffin-Thomas <cavanst@outlook.com>
2020-11-12 09:58:46 +0000

    general project cleanup

--- tests/unit/InputHandlerTest.php
+++ tests/unit/InputHandlerTest.php
@@ -3,8 +3,19 @@
 use PHPUnit\Framework\TestCase;
 require_once "classes/InputHandler.php";
 
+/**
+ * Class InputHandlerTest
+ *
+ * @author Cavan Scoffin-Thomas
+ */
 class InputHandlerTest extends TestCase
 {
+    /**
+     * Check InputHandler returns an array
+     *
+     * @author Cavan Scoffin-Thomas
+     * @test
+     */
     public function testJsonToArrayMakesArray()
     {
         $inputString = "[{\"from\": \"Adolfo Suárez Madrid–Barajas Airport, Spain\",\"to\": \"London Heathrow, UK\"},{\"from\": \"Fazenda São Francisco Citros, Brazil\",\"to\": \"São Paulo–Guarulhos International Airport, Brazil\"},{\"from\": \"London Heathrow, UK\",\"to\": \"Loft Digital, London, UK\"},{\"from\": \"Porto International Airport, Portugal\",\"to\": \"Adolfo Suárez Madrid–Barajas Airport, Spain\"},{\"from\": \"São Paulo–Guarulhos International Airport, Brazil\",\"to\": \"Porto International Airport, Portugal\"}]";
@@ -12,6 +23,12 @@ class InputHandlerTest extends TestCase
         $this->assertIsArray($actualArray);
     }
 
+    /**
+     * Check InputHandler adds the correct Journey object to an array
+     *
+     * @author Cavan Scoffin-Thomas
+     * @test
+     */
     public function testJsonToArrayMakesJourneyObject()
     {
         $inputString = "[{\"from\": \"Adolfo Suárez Madrid–Barajas Airport, Spain\",\"to\": \"London Heathrow, UK\"},{\"from\": \"Fazenda São Francisco Citros, Brazil\",\"to\": \"São Paulo–Guarulhos International Airport, Brazil\"},{\"from\": \"London Heathrow, UK\",\"to\": \"Loft Digital, London, UK\"},{\"from\": \"Porto International Airport, Portugal\",\"to\": \"Adolfo Suárez Madrid–Barajas Airport, Spain\"},{\"from\": \"São Paulo–Guarulhos International Airport, Brazil\",\"to\": \"Porto International Airport, Portugal\"}]";
@@ -44,6 +61,12 @@ class InputHandlerTest extends TestCase
         $this->assertEquals($journey, $actualArray[0]);
     }
 
+    /**
+     * Check InputHandler creates multiple Journey objects in an array
+     *
+     * @author Cavan Scoffin-Thomas
+     * @test
+     */
     public function testJsonToArrayMakesMultipleJourneyObjects()
     {
         $firstArg = "[{\"from\": \"Adolfo Suárez Madrid–Barajas Airport, Spain\",\"to\": \"London Heathrow, UK\"},{\"from\": \"Fazenda São Francisco Citros, Brazil\",\"to\": \"São Paulo–Guarulhos International Airport, Brazil\"},{\"from\": \"London Heathrow, UK\",\"to\": \"Loft Digital, London, UK\"},{\"from\": \"Porto International Airport, Portugal\",\"to\": \"Adolfo Suárez Madrid–Barajas Airport, Spain\"},{\"from\": \"São Paulo–Guarulhos International Airport, Brazil\",\"to\": \"Porto International Airport, Portugal\"}]";
--- tests/unit/JourneyTest.php
+++ tests/unit/JourneyTest.php
@@ -3,8 +3,18 @@
 use PHPUnit\Framework\TestCase;
 require_once "classes/Journey.php";
 
+/**
+ * Class JourneyTest
+ *
+ * @author Cavan Scoffin-Thomas
+ */
 class JourneyTest extends TestCase
 {
+    /**
+     * Mock journey steps
+     *
+     * @var array
+     */
     private $steps = [
         [
             "from"=> "Adolfo Suárez Madrid–Barajas Airport, Spain",
@@ -28,6 +38,12 @@ class JourneyTest extends TestCase
         ],
     ];
 
+    /**
+     * Check the Journey is constructed with the correct start point
+     *
+     * @author Cavan Scoffin-Thomas
+     * @test
+     */
     public function testJourneyStartPoint()
     {
         $expectedStartPoint = [
@@ -39,6 +55,12 @@ class JourneyTest extends TestCase
         $this->assertEquals($expectedStartPoint, $journey->getStartPoint());
     }
 
+    /**
+     * Check the Journey is constructed with the correct end point
+     *
+     * @author Cavan Scoffin-Thomas
+     * @test
+     */
     public function testJourneyEndPoint()
     {
         $expectedEndPoint = [
@@ -50,5 +72,59 @@ class JourneyTest extends TestCase
         $this->assertEquals($expectedEndPoint, $journey->getEndPoint());
     }
 
-    // TODO test getItinerary() and getJourney()
+    /**
+     * Check Journey returns the correct itinerary
+     *
+     * @author Cavan Scoffin-Thomas
+     * @test
+     */
+    public function testGetItinerary()
+    {
+        $expectedItinerary = [
+            "Fazenda São Francisco Citros, Brazil",
+            "São Paulo–Guarulhos International Airport, Brazil",
+            "Porto International Airport, Portugal",
+            "Adolfo Suárez Madrid–Barajas Airport, Spain",
+            "London Heathrow, UK",
+            "Loft Digital, London, UK",
+        ];
+
+        $journey = new Journey($this->steps);
+        $this->assertEquals($expectedItinerary, $journey->getItinerary());
+    }
+
+    /**
+     * Check Journey correctly orders the journey steps
+     *
+     * @author Cavan Scoffin-Thomas
+     * @test
+     */
+    public function testGetJourney()
+    {
+        $expectedJourney = [
+            [
+                "from"=> "Fazenda São Francisco Citros, Brazil",
+                "to" => "São Paulo–Guarulhos International Airport, Brazil",
+            ],
+            [
+                "from"=> "São Paulo–Guarulhos International Airport, Brazil",
+                "to" => "Porto International Airport, Portugal",
+            ],
+            [
+                "from"=> "Porto International Airport, Portugal",
+                "to" => "Adolfo Suárez Madrid–Barajas Airport, Spain",
+            ],
+            [
+                "from"=> "Adolfo Suárez Madrid–Barajas Airport, Spain",
+                "to" => "London Heathrow, UK",
+            ],
+            [
+                "from"=> "London Heathrow, UK",
+                "to" => "Loft Digital, London, UK",
+            ],
+        ];
+
+        $journey = new Journey($this->steps);
+        $this->assertEquals($expectedJourney, $journey->getJourney());
+    }
 }
