2748be6f8410df5972365f7190a88b4fa0c5c832
Cavan Scoffin-Thomas <cavanst@outlook.com>
2020-11-12 08:49:44 +0000

    added test for handling multiple args

--- tests/unit/InputHandlerTest.php
+++ tests/unit/InputHandlerTest.php
@@ -44,5 +44,51 @@ class InputHandlerTest extends TestCase
         $this->assertEquals($journey, $actualArray[0]);
     }
 
-    // TODO test when jsonToArray is given JSON for 2 journeys
+    public function testJsonToArrayMakesMultipleJourneyObjects()
+    {
+        $firstArg = "[{\"from\": \"Adolfo Suárez Madrid–Barajas Airport, Spain\",\"to\": \"London Heathrow, UK\"},{\"from\": \"Fazenda São Francisco Citros, Brazil\",\"to\": \"São Paulo–Guarulhos International Airport, Brazil\"},{\"from\": \"London Heathrow, UK\",\"to\": \"Loft Digital, London, UK\"},{\"from\": \"Porto International Airport, Portugal\",\"to\": \"Adolfo Suárez Madrid–Barajas Airport, Spain\"},{\"from\": \"São Paulo–Guarulhos International Airport, Brazil\",\"to\": \"Porto International Airport, Portugal\"}]";
+        $secondArg = "[{\"from\": \"London Heathrow, UK\",\"to\": \"Loft Digital, London, UK\"},{\"from\": \"Adolfo Suárez Madrid–Barajas Airport, Spain\",\"to\": \"London Heathrow, UK\"},{\"from\": \"Nairobi, Kenya\",\"to\": \"Adolfo Suárez Madrid–Barajas Airport, Spain\"}]";
+
+        $firstSteps = [
+            ["from"=> "Adolfo Suárez Madrid–Barajas Airport, Spain",
+                "to" => "London Heathrow, UK",
+            ],
+            [
+                "from"=> "Fazenda São Francisco Citros, Brazil",
+                "to" => "São Paulo–Guarulhos International Airport, Brazil",
+            ],
+            [
+                "from"=> "London Heathrow, UK",
+                "to" => "Loft Digital, London, UK",
+            ],
+            [
+                "from"=> "Porto International Airport, Portugal",
+                "to" => "Adolfo Suárez Madrid–Barajas Airport, Spain",
+            ],
+            [
+                "from"=> "São Paulo–Guarulhos International Airport, Brazil",
+                "to" => "Porto International Airport, Portugal",
+            ],
+        ];
+
+        $secondSteps = [
+            [
+                "from"=> "London Heathrow, UK",
+                "to" => "Loft Digital, London, UK",
+            ],
+            [
+                "from"=> "Adolfo Suárez Madrid–Barajas Airport, Spain",
+                "to" => "London Heathrow, UK",
+            ],
+            [
+                "from"=> "Nairobi, Kenya",
+                "to" => "Adolfo Suárez Madrid–Barajas Airport, Spain",
+            ],
+        ];
+
+        $firstJourney = new Journey($firstSteps);
+        $secondJourney = new Journey($secondSteps);
+        $actualArray = InputHandler::jsonToArray([$firstArg, $secondArg]);
+        $this->assertEquals([$firstJourney, $secondJourney], $actualArray);
+    }
 }
